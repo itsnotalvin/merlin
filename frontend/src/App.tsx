67ff847f53a50655257809cd068638f1d4a73abb
Alvin Yip <alvin.y97@gmail.com>
2024-10-20 14:20:39 +1100

    added login component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <div className="container">
       <Register />
+      <Login />
     </div>
   )
-  
+
 };
 
 export default App;
